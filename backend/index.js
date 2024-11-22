6b761541c9b7e2561345f3473402457b007f1552
“홍길동” <zxz7648@gmail.com>
2024-11-22 15:17:35 +0900

    서버 점검 안내

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 4000
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+ // res.send('<h1>Hello World!</h1>')
+ res.sendFile(path.join(__dirname, "notice.html")); // 서버 점검을 알리는 웹문서를 클라이언트에 전송하는 명령
 })
 
 app.listen(port, () => {
